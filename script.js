287dc8c4733df9339859f341e23f3f828b60d3a8
sureshvaddegithub <116425651+sureshvaddegithub@users.noreply.github.com>
2023-04-12 10:56:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+let team = players;
+let team1 = players.map((element) =>{
+	return element;
+})
 
 window.players = players;
 window.person = person;
